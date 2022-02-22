d7e612dd3fc90c75365973e17cc935b6ff0e0f45
nareshn <nareshn@pronixinc.com>
2022-02-22 11:42:18 +0530

    Add project files.

---
+++ IdentityApp/Controllers/AuthenticateController.cs
@@ -0,0 +1,142 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
+
+        public AuthenticateController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(RegistrationModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User alredy exists" });
+            }
+            else
+            {
+                IdentityUser newUser = new IdentityUser()
+                {
+                    UserName = model.Username,
+                    Email = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                };
+
+                var result = await userManager.CreateAsync(newUser, model.Password);
+                if (!result.Succeeded)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed!!" + result.Errors });
+
+                return Ok(new Response { Status = "Success", Message = "User created successfully" });
+            }
+
+        }
+
+        [HttpPost]
+        [Route("register-ad")]
+        public async Task<IActionResult> RegisterAdmin(RegistrationModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User alredy exists" });
+            }
+            else
+            {
+                IdentityUser newUser = new IdentityUser()
+                {
+                    UserName = model.Username,
+                    Email = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                };
+
+                var result = await userManager.CreateAsync(newUser, model.Password);
+                if (!result.Succeeded)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed!!" });
+
+                if (!await roleManager.RoleExistsAsync("Admin"))
+                    await roleManager.CreateAsync(new IdentityRole("Admin"));
+
+                if (await roleManager.RoleExistsAsync("Admin"))
+                    await userManager.AddToRoleAsync(newUser, "Admin");
+                return Ok(new Response { Status = "Success", Message = "User created successfully" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user == null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User not found" });
+
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.SecurityStamp),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                foreach (var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                var token = GetToken(authClaims);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo,
+                });
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return token;
+        }
+    }
+}
---
+++ IdentityApp/Models/RegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityApp.Models
+{
+    public class RegistrationModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
